19bc761a97fde32e71dac84760c064cdcab1ef0b
AndUZUT <TestoweHaslo32>
2020-06-14 23:10:06 +0200

    test2

--- app/src/main/java/com/example/lab04/DodajWpis.java
+++ app/src/main/java/com/example/lab04/DodajWpis.java
@@ -2,10 +2,23 @@ package com.example.lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
+    public void wyslij(View view) {
+        EditText kontrolka =
+                (EditText)findViewById(R.id.editText);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
